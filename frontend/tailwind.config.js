b0e032a6560751ae16d57cb55ef4aaf1d0db9917
yallappajogi07 <159703761+yallappajogi07@users.noreply.github.com>
2026-08-24 22:32:04 +0530

    Fix Tailwind and ESLint configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-/ ** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,4 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
